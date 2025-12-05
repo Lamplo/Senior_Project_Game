949cdeb3fec971f974cb36d76e98cfe48d695cdc
Lamplo <donovanesver@gmail.com>
2025-12-04 19:13:13 -0800

    InputManager in progress
    Completed:
    Set up variables for input
    
    Next Steps:
    Create events to handle inputs

--- Spinner/Assets/Scripts/InputManager.cs
+++ Spinner/Assets/Scripts/InputManager.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+//class organizes data
+/* 
+    FUNCTION DECLARATION
+    // [Accessor] [ReturnType] [Name](Parameters){
+    // Function Body
+    //}
+    //Awake called once when object is enabled
+
+    VAR DECLARATION
+    [Accesor][Type][Name]=[Value];
+
+    VAR ASSIGNMENT
+    [Var_Name] = [Value]
+*/
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-      Debug.Log("Hello World"); 
-    }
+    public static InputManager Instance {get;private set;}
+    private InputActionAsset inputActions;
+    
+    private InputAction selectAction;
+    
+    private void Awake(){
+      // Check Signleton
+      if(Instance == null) Instance = this;
+      else if(Instance != null) {
+        Destroy(this);
+        Debug.LogWarning("Cannot have more than one copy of InputManager");
+      }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Hello World");
+      // Initialze Members (Variables)
+      // By accessing the InputActions variable, call the FindAction var and pass in the select action as the argument.
+      // Assign the output of this function to the SelectionAction var
+
+      selectAction = inputActions.FindAction("Select");
+    }
+  
+    // Create the built in function OnEnable, make it private with no parameters
+    private void OnEnable(){
+      // When this script is enabled, find the Player action map and enabled it to be used
+      inputActions.FindActionMap("Player").Enable();
+    }
+    // When this script is disabled, find the Player action map and disable it
+    private void OnDisable(){
+      inputActions.FindActionMap("Player").Disable();
     }
 }
