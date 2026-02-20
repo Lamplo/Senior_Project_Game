31126a752a4597b3de142ad7df881f845f03d8a2
Lamplo <donovanesver@gmail.com>
2026-02-19 16:06:28 -0800

    First Trinket/Consumable
    Made script for trinkets to take from
    Made trinket interface script
    Made script for piggy bank consumable
    
    Next Streps
    Refactor the input handling for consumable
    make the other base trinkets

--- Spinner/Assets/Scripts/RouletteWheel.cs
+++ Spinner/Assets/Scripts/RouletteWheel.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 public class RouletteWheel : MonoBehaviour
 {
      
-     public static RouletteWheel Instance{get; private set;}
+    public static RouletteWheel Instance{get; private set;}
      
-     private List<int> rouletteWheelList = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+    private List<int> rouletteWheelList = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+    [SerializeField]
+    private int pointMulitplier = 1;
 
-     
-     private void Awake(){
+    private void Awake(){
       // Check Singleton
       if(Instance == null) Instance = this;
       else if(Instance != null) {
@@ -23,9 +24,11 @@ public class RouletteWheel : MonoBehaviour
       // Randomly select an element from the roulette Wheel list, store that element in a variable
       int randomSlot = GetRandomElementFromList<int>(rouletteWheelList);
       // Print that output to the console
-      Debug.Log(randomSlot);
+      Debug.Log(randomSlot * pointMulitplier);
       // Add points to the score
-      ScoreManager.Instance.EarnPoints(randomSlot);
+      ScoreManager.Instance.EarnPoints(randomSlot * pointMulitplier);
+      // Reset the multiplier
+      pointMulitplier = 1;
     }
 
     private T GetRandomElementFromList<T>(List<T> list)
@@ -42,5 +45,10 @@ public class RouletteWheel : MonoBehaviour
       // Use that random to select an element from the list by its index
       return list[randomIndex];
       // Return that element from the list
+    }
+
+    public void ApplyMultiplier(int newMultiplier){
+      // Takes in an int and sets the multipler for the next spin to that number
+      pointMulitplier = newMultiplier;
     }
 }
