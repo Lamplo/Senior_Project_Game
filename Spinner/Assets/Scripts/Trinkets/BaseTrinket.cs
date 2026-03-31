fe44c7042599b504a3481912b1d2e4de60e5c461
Lamplo <donovanesver@gmail.com>
2026-03-31 16:45:43 -0700

    Refactor in progress
    New Classes: Wheel Context, Wheel Slot, Wheel Modifier
    
    Next Steps: concrete trinket classes

--- Spinner/Assets/Scripts/Trinkets/BaseTrinket.cs
+++ Spinner/Assets/Scripts/Trinkets/BaseTrinket.cs
@@ -4,35 +4,18 @@ public enum TrinketType{
     Consumable
 }
 
+
 public abstract class BaseTrinket : MonoBehaviour, Itrinket
 {
     // Declare common attributes and functions across all trinkets
 
-    protected TrinketData data; // Ref to scriptable object
+    [SerializeField] TrinketData data; // Ref to scriptable object
 
     [SerializeField] protected TrinketType trinketType = TrinketType.Consumable;
 
-    protected void Start()
-    {
-        // Check the type of consumeable and handle its logic
-        switch (trinketType)
-        {
-            case TrinketType.Passive:
-            // If the trinket is a passive, apply its effect immediately
-            ApplyTrinketEffect();
-            break;
-            case TrinketType.Consumable:
-            // Handle consumeable logic
-            break;
-        }
-    }
+    public abstract void ModifyWheel(WheelContext context);
 
-    public virtual void ApplyTrinketEffect()
-    {
-        Debug.Log($"{data.trinketName}");
-    }
-
-    public TrinketType GetType()
+    public virtual TrinketType GetType()
     {
         return trinketType;
     }
--- Spinner/Assets/Scripts/Trinkets/PlayerInventory.cs
+++ Spinner/Assets/Scripts/Trinkets/PlayerInventory.cs
@@ -3,21 +3,4 @@ using System.Collections.Generic;
 
 public class PlayerInventory : MonoBehaviour
 {
-    [SerializeField, Tooltip("The list of trinkets the player has equipped")]
-    private List<BaseTrinket> equippedTrinkets = new List<BaseTrinket>();
-    
-    private void Start(){
-        // Check if any trinkets are passive buffs
-        // For each trinket in equipped Trinkets
-        foreach (BaseTrinket trinket in equippedTrinkets){
-            // Check if it is a passive
-            if (trinket.GetType() == TrinketType.Passive){
-                //  trigger passive effect
-                trinket.ApplyTrinketEffect();
-            }
-            else if (trinket.GetType() == TrinketType.Consumable){
-                // Handle logic, or ignore
-            }
-        }
-    }
 }
