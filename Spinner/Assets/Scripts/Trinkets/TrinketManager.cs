fb7865b877ec00c68d75e4648610596cb28a4de0
Lamplo <donovanesver@gmail.com>
2026-04-07 16:49:13 -0700

    Trinket Buttons in progress
    Made TrinketButton, remade trinket UI as button.
    
    Next Steps: Configure trinket modify wheel, recreate trinkets as UI buttons.

---
+++ Spinner/Assets/Scripts/Trinkets/TrinketManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TrinketManager : MonoBehaviour
+{
+    public static TrinketManager Instance {get;private set;}
+
+    // A list of all trinket buttons
+    [SerializeField] List<TrinketButton> _trinketButtons = new List<TrinketButton>();
+
+    private void Awake(){
+        // Check Singleton
+        if(Instance == null) Instance = this;
+        else if(Instance != null) {
+            Destroy(this);
+            Debug.LogWarning("Cannot have more than one copy of InputManager");
+        }
+    }
+
+    public void InitializeButtons(WheelContext cxt){
+        if (cxt == null){
+            Debug.LogError("wheel context is null!");
+            return;
+        }
+
+        foreach (TrinketButton button in _trinketButtons){
+            button.Initialize(cxt);
+        }
+    }
+
+    public void RegisterButton(TrinketButton button){
+        _trinketButtons.Add(button);
+    }
+
+
+}
